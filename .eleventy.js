ed1da661fe301b81d78f0c5655440dc8040d1eb0
Alena Batitskaia <batickaya.a@gmail.com>
2024-06-11 23:46:48 +0100

    Custom domain: CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("CNAME");
   return {
     passthroughFileCopy: true
   }
