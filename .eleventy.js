69a4637c8fc2b29e6ca610f0df2b7aebf94624f4
Alena Batitskaia <batickaya.a@gmail.com>
2024-06-11 23:31:48 +0100

    Cleaning

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("images/*");
+  eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("fonts");
   return {
     passthroughFileCopy: true
