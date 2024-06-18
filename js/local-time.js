16fe853f3d7547bfb05108498f4320ce0a8a67e2
solarrust <solarrust@users.noreply.github.com>
2024-06-18 10:36:33 +0000

    deploy: 51f6eb6531f07fec1e9b4ac7efc7c22db9de09a5

--- js/local-time.js
+++
@@ -1,8 +0,0 @@
-const localTime = document.querySelector('.local__time')
-
-function timedUpdate () {
-  localTime.innerText = moment().format('MMMM Do YYYY, h:mm a')
-  setTimeout(timedUpdate, 10000);
-}
-
-timedUpdate()
